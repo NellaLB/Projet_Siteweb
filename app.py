3cd951b7cf8b2c1a6a60d8f85fb1d7078bc41f97
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-14 11:56:18 +0200

    Site Web

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, abort, render_template
+from markupsafe import escape
+import datetime
+
+app = Flask(__name__) #Instance
+
+@app.route('/') #Décorateur : Instruction définissant URL
+@app.route('/index/')
+def hello():
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+
+@app.route('/addition/<int:nb1>/<int:nb2>/')
+def addition(nb1,nb2):
+    try:
+        return '<h2>{}</h2>'.format(nb1 + nb2)
+    except IndexError:
+        abort(404)
