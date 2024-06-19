ef1272b6bf435318815f6908c296d517ba6860fe
NellaLB <Ornella.LE-BRAS@eleve.egd.mg>
2024-06-19 10:14:46 +0200

    SiteWeb

--- app.py
+++ app.py
@@ -1,19 +1,52 @@
-from flask import Flask, abort, render_template
+from flask import Flask, abort, render_template, session, app, request, url_for,redirect
 from markupsafe import escape
 import datetime
 
 app = Flask(__name__) #Instance
+app.secret_key =  b'_5#y2L"F4Q8z\n\xec]/' #Eviter piratage
+
 
 @app.route('/') #Décorateur : Instruction définissant URL
 @app.route('/index/')
 def hello():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    if ('username' in session) and ('password' in session):
+        return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    return redirect(url_for('login'))
+
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+        if session['username'] == 'John' and session['password'] == '1234':
+            return redirect(url_for('hello'))
+        else:
+            return redirect(url_for('login'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=text name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout/')
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    return redirect(url_for('hello'))
+
+
+
+
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
 
-@app.route('/capitadd/<word>/')
+@app.route('/capitaze/<word>/')
 def capitalize(word):
     return '<h1>{}</h1>'.format(escape(word.capitalize()))
 
